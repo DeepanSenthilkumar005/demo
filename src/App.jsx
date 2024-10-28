8745aaf59659790d65afb44a5a8ca43627783eaf
Deepan Senthilkumar <164661074+DeepanSenthilkumar005@users.noreply.github.com>
2024-10-28 20:44:59 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Result from './components/result.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App m-auto">
+      
+      <Result />
+    </div>
+  );
+}
+
+export default App;
